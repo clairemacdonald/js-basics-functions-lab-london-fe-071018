e3ed8cbfa99a326b679fc7d5b34ac929b7ed2119
clairemacdonald <claire.macdonald@blis.com>
2018-08-14 14:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks (location) {
+  const hq = 42;
+  return location > hq ? location%hq : hq %location;
+}
+
+
+function distanceFromHqInFeet (location) {
+ return distanceFromHqInBlocks(location)*264;
+}
+
+function distanceFromLocationInBlocks (location, destination) {
+  return location > destination ? location%destination : destination %location;
+}
+function distanceTravelledInFeet(location, destination) {
+  return distanceFromLocationInBlocks(location, destination)*264;
+}
+
+function calculateFarePrice (location, destination) {
+  const distance = distanceTravelledInFeet(location, destination);
+  if (distance <= 400){
+    return "free"; 
+    
+  } if else (distance > 400 && distance < 2000)
+  return (distance-400) * 2;
+  
+   
+} 
